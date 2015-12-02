0b7a6a5d7f7d07bded1fc2f87e6224e70b4aaabc
Kristian Damian <kristiandamian@gmail.com>
2015-12-02 16:14:49 -0700

    Se agrega clave para Xamarin insights

---
+++ Quinieleros.BO/Insights.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Quinieleros.BO
+{
+    public static class Insights
+    {
+        public  const string ApiKey = "6e6018b5de20637dcf60c77c45509772fb7beea0";
+	}
+}
+
--- Quinieleros.iOS/Main.cs
+++ Quinieleros.iOS/Main.cs
@@ -7,7 +7,7 @@ namespace Quinieleros.iOS
 		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
-			Xamarin.Insights.Initialize (XamarinInsights.ApiKey);
+			Xamarin.Insights.Initialize (BO.Insights.ApiKey);
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
